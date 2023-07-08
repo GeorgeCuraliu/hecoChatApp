5ae166f99da9baaec277c9db8d20cca6a4a2b46c
AlexLascau <115173000+AlexLascau@users.noreply.github.com>
2023-07-08 17:58:33 +0300

    primu push

---
+++ src/Authentification/auth.js
@@ -0,0 +1,21 @@
+import "../styles/auth.css";
+
+const LoginPage = () => {
+    return(
+        <div>
+            <header>
+                <p>HECO Chat</p>
+            </header>
+            <div className="body">
+                <img />
+                <div>Username</div>
+                <input />
+                <div>Password</div>
+                <input />
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
